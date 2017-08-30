990fcc14dcae28e9dc307e642bf41b05b30533b1
sue martinez <sueherrera30@gmail.com>
2017-08-30 18:32:06 +0000

    ordering by rating,from biggest to smallest in console

--- js/main.js
+++ js/main.js
@@ -1,39 +1,54 @@
+var url = "https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json";
+
 /*get elements from DOM*/
 
 var alphabethBtn = document.getElementById("alphabeth");
 var ratingBtn = document.getElementById("rating");
 
-/*alphabethBtn.addEventListener('click', orderingAlphabetically);*/
-ratingBtn.addEventListener('click', getInformation);
+alphabethBtn.addEventListener('click',OrderingAlphabetically);
+ratingBtn.addEventListener('click', OrderingByRating);
 
 
-/*making AJAX request*/
+				/*making AJAX request*/
+/*ordering alphabetically*/
 var nameArray = [];
-function getInformation (){
-var url = "https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json";
+function OrderingAlphabetically(){
 var theRequest = new XMLHttpRequest();
 theRequest.open('GET' ,url);
 theRequest.onload = function(){
 	var info = JSON.parse(theRequest.responseText);
 	info.forEach(function(name){
 	var restaurantName = name.name;
-	nameArray.push(name.name)	
-		
+	nameArray.push(name.name)			
 	})
- 
 	nameArray.sort();
     console.log(nameArray);	
 };
-theRequest.send();
+theRequest.send();	
+};
 
+/*ordering by rating*/
+var generalRatingArray = [];
+var ratingAndNameArray = [];
+function OrderingByRating(){
+var theRequest = new XMLHttpRequest();
+theRequest.open('GET' ,url);
+theRequest.onload = function(){
+	var info = JSON.parse(theRequest.responseText);
+	info.forEach(function(response){
+	var rating = response.rating;
+	var name = response.name;
 	
+	ratingAndNameArray.push(rating);
+	ratingAndNameArray.sort(function(a,b){ return b - a});		
+	})
+		console.log(ratingAndNameArray);
+	generalRatingArray.push(ratingAndNameArray);
+	console.log(generalRatingArray);  	
+};
+theRequest.send();	
 };
 
 
 
-/*ordering alphabetically*/
-/*
-function orderingAlphabetically(info){
-	}
-*/
 
